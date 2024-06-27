96d1398c20c0c2e0d04f9fced25b22596d59ac48
fernandodafonseca <fernandoj.dafonseca@gmail.com>
2024-06-27 19:49:04 -0300

    AYUDA del paquete

---
+++ R/AnalisisCombinadoR.R
@@ -0,0 +1,8 @@
+#' AnalisisCombinadoR: Un paquete para análisis combinado de ensayos
+#'
+#' El paquete AnalisisCombinadoR proporciona diversas funciones para realizar
+#' análisis combinados de datos.
+#'
+#' @docType package
+#' @name AnalisisCombinadoR
+NULL
