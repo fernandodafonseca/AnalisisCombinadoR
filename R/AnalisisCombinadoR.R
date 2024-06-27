f1ef8d6f425307323a9437f3c67cdefedf3cc9f1
fernandodafonseca <fernandoj.dafonseca@gmail.com>
2024-06-27 19:53:32 -0300

    Edicion de la AYUDA

--- R/AnalisisCombinadoR.R
+++ R/AnalisisCombinadoR.R
@@ -1,7 +1,9 @@
 #' AnalisisCombinadoR: Un paquete para análisis combinado de ensayos
 #'
 #' El paquete AnalisisCombinadoR proporciona diversas funciones para realizar
-#' análisis combinados de datos.
+#' análisis combinados de ensayos. Ademas contiene funciones para realizar la
+#' verificacion grafica de los supuestos de normalidad y de homogeneidad de
+#' de los residuos.
 #'
 #' @docType package
 #' @name AnalisisCombinadoR
